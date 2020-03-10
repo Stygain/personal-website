95bc67831d1ef035882c62fef9861242609f7e54
Adam Barton <adamb6102@gmail.com>
2020-03-10 16:39:25 -0700

    Made content wider on smaller screens.

--- src/Content/Personal.js
+++ src/Content/Personal.js
@@ -9,6 +9,7 @@ function Personal(props) {
   const styling = css`
     ${'' /* border: 1px solid green; */}
 
+    margin: 0px 10px;
     margin-top: 50vh;
 
     ${'' /* @import url('https://fonts.googleapis.com/css?family=Oxanium&display=swap'); */}
@@ -40,8 +41,18 @@ function Personal(props) {
       font-size: 18px;
     }
 
+    p:nth-child(1) {
+      margin-top: 0px;
+    }
+
     a {
       color: rgb(14, 236, 131);
+    }
+
+    @media (max-width: 800px) {
+      .about-text-container {
+        width: 60%;
+      }
     }
   `;
 
@@ -50,7 +61,7 @@ function Personal(props) {
       <SectionHeader content="About Me" margin="140" />
       <div className="personal content-container">
         <div className="about-text-container">
-          <p>I am a softare engineer from Portland, Oregon who enjoys creating things of all kinds, whether that be software, woodworking, computers, or more! I am a recent graduate of <a href="https://oregonstate.edu/">Oregon State University</a> where I received a BS in Computer Science with a focus in security.</p>
+          <p>I am a software engineer from Portland, Oregon who enjoys creating things of all kinds, whether that be software, woodworking, computers, or more! I am a recent graduate of <a href="https://oregonstate.edu/">Oregon State University</a> where I received a BS in Computer Science with a focus in security.</p>
           <p>After graduating Oregon State I joined the Windows endpoint team at <a href="https://www.carbonblack.com/">VMware Carbon Black</a> where I get to work on core functionality of antivirus software.</p>
         </div>
         <ImageContainer />
